bdcaaf285cba8497ab114c04d63127f93d479f6a
naitosama <naitosama@qq.com>
2024-03-05 17:00:36 +0800

    boss图标使用cache

--- server/bossPic.go
+++ server/bossPic.go
@@ -5,12 +5,14 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"path/filepath"
+	"pcrclanbattle_server/common"
+	"pcrclanbattle_server/db"
 	"strconv"
 )
 
 func UploadBossPic(c *gin.Context) {
 	authority, _ := c.Get("user_authority")
-	if authority.(int) == 0 {
+	if authority == "0" {
 		c.JSON(http.StatusForbidden, gin.H{
 			"result": "insufficient permissions",
 		})
@@ -29,7 +31,7 @@ func UploadBossPic(c *gin.Context) {
 		})
 		return
 	}
-	bossID, err := strconv.ParseInt(c.Param("boss"), 10, 8)
+	bossID, err := strconv.Atoi(c.Query("boss"))
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"result": "param boss is not int",
@@ -44,6 +46,17 @@ func UploadBossPic(c *gin.Context) {
 		})
 		return
 	}
+	//
+	lock.Lock()
+	defer lock.Unlock()
+	newBoss := db.Cache.Bosses[bossID-1]
+	newBoss.PicETag, err = common.CalculateETag(dst)
+	if err != nil {
+		common.Logln(2, err)
+		return
+	}
+	err = renewBoss(newBoss)
+
 	c.JSON(http.StatusOK, gin.H{
 		"result": "ok",
 	})
